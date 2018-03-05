59fac0055d215e63232be64ccf9f059a34c4cd95
Peter Hudak <hudak_epam@epam.com>
2018-03-05 15:05:36 +0100

    Project setup

---
+++ src/main/java/com/pedritto/testlab/TestLabServer/TestLabServerApplication.java
@@ -0,0 +1,12 @@
+package com.pedritto.testlab.TestLabServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLabServerApplication.class, args);
+	}
+}
